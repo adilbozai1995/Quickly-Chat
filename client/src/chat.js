58804605108cf7f5562c8df43b70cd6738c3d312
Hadeed Noshab <hnoshab@pal-nat186-11-204.itap.purdue.edu>
2019-04-25 00:17:19 -0400

    chat page basics

--- client/src/chat.js
+++ client/src/chat.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './chat.css';
 
 class chat extends Component {
+
+
+  sendClicked(){
+
+    
+  }
     render() {
      return (
        <div className="Chat">
+
+        <div className = "input_holder">
+        
+        <div className = "chat_bubbles">
+        
+        
+        
+        
+        </div>
+        
+        <textarea className = "chat_input" id = "user_message"/>
+        <button OnClick={() => this.sendClicked()} className = "chat_send">SEND</button>
+        
+        </div>
+
+        
+
 
         
       </div>
