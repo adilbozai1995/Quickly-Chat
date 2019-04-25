bac11e55d1adcaeeec33217d8f68e23a04abdc75
QuantX <sam.e.deutsch@gmail.com>
2019-04-24 22:38:30 -0400

    Added login functionality

--- server/main.js
+++ server/main.js
@@ -20,6 +20,40 @@ app.get('/', function (req, res) {
     res.send( out )
 });
 
+// Connect to the database as the insecure bot. use for reading data
+var db1 = mysql.createConnection({
+    host: 'thestrugglingengineer.com',
+    port: '3306',
+    user: 'service',
+    password: '9nQzDKe2kDs+y7o2VArScg==',
+    database: 'Anthologist'
+});
+
+db1.connect(function(err) {
+    if (err) {
+        return console.error('MySQL error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server as: service');
+});
+global.sqlcon = db1;
+
+// Connect to the database as the secure bot. use for writing data.
+var db2 = mysql.createConnection({
+    host: 'thestrugglingengineer.com',
+    port: '3306',
+    user: 'serviceSecure',
+    password: 'C+n9NscBcHbYPmAp6cN5Hw==',
+    database: 'Anthologist'
+});
+
+db2.connect(function(err) {
+    if (err) {
+        return console.error('MySQL error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server as: serviceSecure');
+});
+global.sqlsec = db2;
+
 app.listen(port, (err) => {
     if ( err ) { console.log( err ); };
     console.log(`Quickly Chat backend server listening on port ${port}!`);
--- server/routes/api/login.js
+++ server/routes/api/login.js
@@ -5,10 +5,114 @@ const isuuid = require("is-uuid")
 module.exports = (app) => {
 
     app.post('/api/signup', function(req, res) {
+        if(!req.body
+        || !req.body.username
+        || !req.body.password)
+        {
+            console.log( "signup: missing field" );
+            return res.sendStatus(400)
+        }
 
+        const username = req.body.username
+        const password = req.body.password
+
+        if ( username.length > 50 )
+        {
+            console.log( "signup: username too long" );
+            return res.sendStatus(400)
+        }
+
+        if ( password.length > 32 )
+        {
+            console.log( "signup: password too long" );
+            return res.sendStatus(400)
+        }
+
+        const salt = crypto.randomBytes(32);
+        const hashpass = crypto.pbkdf2Sync( password, salt, 100000, 32, "sha512" );
+        const token = crypto.randomBytes(32).toString('hex');
+
+        sqlsec.query( "INSERT INTO cs252users (username, password, salt, token) VALUES (?, ?, ?, ?);",
+        [ username, hashpass, salt, token ], function( err, rsql )
+        {
+            if ( err )
+            {
+                if ( err.code === "ER_DUP_ENTRY" )
+                {
+                    console.log( "signup: failed to create account, username taken: " + username )
+                    res.json({"status":"fail","reason":"username already taken"})
+                }
+                else
+                {
+                    console.log( "signup: sql_error: ", err )
+                    res.json({"status":"fail","reason":"un-caught sql error"})
+                }
+            }
+            else
+            {
+                console.log( "signup: created account for user: " + username )
+                res.json({"status":"okay","token":token})
+            }
+        });
     });
 
     app.post('/api/login', function(req, res) {
+        if(!req.body
+        || !req.body.username
+        || !req.body.password)
+        {
+            console.log( "login: missing field" );
+            return res.sendStatus(400)
+        }
 
+        const username = req.body.username
+        const password = req.body.password
+
+        if ( username.length > 50 )
+        {
+            console.log( "login: username too long" );
+            return res.sendStatus(400)
+        }
+
+        if ( password.length > 32 )
+        {
+            console.log( "login: password too long" );
+            return res.sendStatus(400)
+        }
+
+        sqlcon.query( "SELECT password, salt FROM cs252users WHERE username=?;",
+        [ username ], function( err, rsql )
+        {
+            if ( err )
+            {
+                console.log( "login: sql_error: ", err );
+                res.json({"status":"fail","reason":"un-caught sql error"});
+            }
+            else if ( rsql.length <= 0 )
+            {
+                console.log( "login: no account with username: " + username )
+                res.json({"status":"fail","reason":"unknown account"})
+            }
+            else
+            {
+                const salt = Buffer.from( rsql[0].salt, 'hex' )
+                const hashpass = crypto.pbkdf2Sync( password, salt, 100000, 32, "sha512" )
+
+                if ( hashpass.toString('hex') === rsql[0].password )
+                {
+                    const token = crypto.randomBytes(32).toString('hex');
+
+                    sqlsec.query( 'UPDATE cs252users SET token=? WHERE username=?;', [ token, username ] );
+
+                    console.log( "login: correct password for user: " + username );
+                    res.json({"status":"okay","token":token})
+                }
+                else
+                {
+                    console.log( "login: incorrect password for user: " + username );
+                    res.json({"status":"fail","reason":"incorrect password"})
+                }
+            }
+        });
     });
 }
