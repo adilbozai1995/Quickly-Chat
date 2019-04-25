7983834cf2efca38460120f5441572f75a8a701b
Adil Uddin Khan Bozai <adilbozai@pal-nat186-165-229.itap.purdue.edu>
2019-04-25 00:22:35 -0400

    loginPage things

--- client/src/login.js
+++ client/src/login.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import { Link,Prompt } from 'react-router-dom';
 import logo from './logo.svg';
 import './login.css';
+import { Button, FormGroup, FormControl, ControlLabel, Form } from "react-bootstrap";
 
 class login extends Component {
+
+  onClickSignup = () => {
+  
+  }
+
+  onClickLogin = () => {
+
+  }
+
   render() {
    return (
-     <div className="App">
-      
+    <div className="login">
+      <Form className="loginForm">
+
+        <Form.Group className="usernameForm">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="username" placeholder="Enter username" id="usernameID"/> 
+        </Form.Group>
+
+        <Form.Group className="usernameForm">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" id="passwordID" />
+        </Form.Group>
+
+        
+        <Button className="button" onClick={() => this.onClickSignup()} id="signupButton" type="submit">Sign-Up</Button>
+        <Button className="button" onClick={() => this.onClickLogin()} id="loginButton" type="submit">Log-In</Button>
+
+      </Form>
     </div>
    );
   }
