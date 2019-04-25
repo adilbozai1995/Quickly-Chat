e7f9e0b870715ccf2677ee781db3e3b80266837b
QuantX <sam.e.deutsch@gmail.com>
2019-04-25 01:29:13 -0400

    Added logout

--- client/src/chat.js
+++ client/src/chat.js
@@ -11,25 +11,37 @@ class chat extends Component {
 
   jumpEndClicked(){
 
-
-  
   }
 
-  logoutClicked(){
+  logoutClicked()
+  {
+      localStorage.account = ""
+      localStorage.token = ""
 
-
+      window.location.replace( "/" )
   }
 
   joinRoomClicked(){
 
-
   }
+
+  componentDidMount()
+  {
+      if ( !localStorage.account || !localStorage.token )
+      {
+          localStorage.account = "";
+          localStorage.token = "";
+
+          window.location.replace("/")
+      }
+  }
+
     render() {
      return (
        <div className="Chat">
-        <button OnClick={() => this.jumpEndClicked()} className = "jump_end">END</button>
-        <button OnClick={() => this.joinRoomClicked()} className = "join_room_btn">JOIN ROOM</button>
-        <button OnClick={() => this.logoutClicked()} className = "logout_btn">logout</button>
+        <button onClick={() => this.jumpEndClicked()} className = "jump_end">END</button>
+        <button onClick={() => this.joinRoomClicked()} className = "join_room_btn">JOIN ROOM</button>
+        <button onClick={() => this.logoutClicked()} className = "logout_btn">logout</button>
         <input className = "room_input" id = "user_message"/>
         <div className = "input_holder">
         
@@ -41,7 +53,7 @@ class chat extends Component {
         </div>
         
         <textarea className = "chat_input" id = "user_message"/>
-        <button OnClick={() => this.sendClicked()} className = "chat_send">SEND</button>
+        <button onClick={() => this.sendClicked()} className = "chat_send">SEND</button>
         
         </div>
 
