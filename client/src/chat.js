949ff638e0b2ac796b12453946be6d45bf120739
Adil Uddin Khan Bozai <adilbozai@pal-nat186-165-229.itap.purdue.edu>
2019-04-24 23:23:57 -0400

    added basic files

---
+++ client/src/chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './chat.css';
+
+class chat extends Component {
+    render() {
+     return (
+       <div className="App">
+        
+      </div>
+     );
+    }
+    
+  }
+
+export default App;
---
+++ client/src/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link,Prompt } from 'react-router-dom';
+import logo from './logo.svg';
+import './login.css';
+
+class login extends Component {
+  render() {
+   return (
+     <div className="App">
+      
+    </div>
+   );
+  }
+  
+}
+
+export default login;
